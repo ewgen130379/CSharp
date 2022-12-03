a89023f729059aadd2fc46694809860e7b11b561
ewgen130379 <118625594+ewgen130379@users.noreply.github.com>
2022-12-03 17:02:15 +0300

    Решили первую задачу 0.Вывести квадрат числа

---
+++ Seminar/Seminar_001/Examp001/Program.cs
@@ -0,0 +1,6 @@
+﻿// 0. Вывести квадрат числа
+Console.WriteLine("start");// начало
+int number = 11;// определили целое число
+int result = number * number;// Вычеслили квадрат числа
+Console.WriteLine(result);// Вывод результата
+Console.WriteLine("end");// окончание
